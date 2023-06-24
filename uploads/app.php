48a7b6a4c90433b2d665ac65de3cfba5421ed258
Nicolas <112518653+Nirclaw@users.noreply.github.com>
2023-06-23 20:17:31 -0500

    Update app.php

--- uploads/app.php
+++ uploads/app.php
@@ -4,22 +4,44 @@ require '../vendor/autoload.php';
 
 $router=new \Bramus\Router\Router();
 
-$router->get('/camper',function(){
+$router->get("/camper", function(){
     $cox = new \App\connect();
-   $res=$cox->con->prepare("SELECT * FROM tb_camper");
-   $res->execute();
-   $res=$res->fetchAll(\PDO::FETCH_ASSOC);
-   echo json_encode($res);
+    $res = $cox->con->prepare("SELECT * FROM tb_camper");
+    $res -> execute();
+    $res = $res->fetchAll(\PDO::FETCH_ASSOC);
+    echo json_encode($res,true);
 });
 
-$router->put('/camper',function(){
-    $_DATA = json_decode(file_get_contents("php://input"),true);
+$router->put("/camper", function(){
+    $_DATA = json_decode(file_get_contents("php://input"), true);
     $cox = new \App\connect();
-    $res=$cox->con->prepare('UPDATE  tb_camper SET nombre = :NOMBRE,edad=EDAD WHERE id=:id;');
-    $res->bindValue("NOMBRE",$_DATA['nom']);
-    $res->bindValue("id",$_DATA['id']);
+    $res = $cox->con->prepare("UPDATE tb_camper SET nombre = :NOMBRE, edad= :EDAD WHERE id =:CEDULA");
+    $res-> bindValue("NOMBRE", $_DATA['nom']);
+    $res-> bindValue("EDAD", $_DATA['edad']); //para editar se debe escribir la sentencia dentro del $_DATA["nom"] es decir { nom: Wilfer, id: 1}
+    $res-> bindValue("CEDULA", $_DATA['id']);
+    $res -> execute();
+    $res = $res->rowCount();
+    echo json_encode($res);
+});
+
+$router -> delete("/camper", function(){
+    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $cox = new \App\connect();
+    $res = $cox->con->prepare("DELETE FROM tb_camper WHERE id =:CEDULA");
+    $res->bindValue("ID", $_DATA["id"]);
     $res->execute();
-    $res= $res->rowCount();
+    $res = $res->rowCount();
+    echo json_encode($res);
+});
+
+$router->post("/camper", function(){
+    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $cox = new \App\connect();
+    $res = $cox->con->prepare("INSERT INTO tb_camper (nombre, edad) VALUES (:NOMBRE, :CEDULA");
+    $res-> bindValue("nombre", $_DATA['nom']); //para editar se debe escribir la sentencia dentro del $_DATA["nom"] es decir { nom: Wilfer, id: 1}
+    $res-> bindValue("edad", $_DATA['edad']); 
+    $res -> execute();
+    $res = $res->rowCount();
     echo json_encode($res);
 });
 
